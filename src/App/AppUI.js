9683f6207533f37578ed068a13ffe590cc19e76c
Sebastian Jacome <sebasjack@Sebastians-MacBook-Air-2.local>
2022-11-14 10:42:15 -0600

    MainMenú-Finished

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {LogInForm} from "../Components/LogInForm";
+import {MainMenu} from "../Components/MainMenu";
 import "./AppUI.css";
 
 const AppUI = () => {
 
     return(
         <div className="container">
-            <LogInForm/>
+            {/*<LogInForm/>*/}
+            <MainMenu/>
         </div>
 
     );
