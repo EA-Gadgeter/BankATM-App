7e0a68daa65d09c9260b6167d60a0b01b173172a
Emiliano Acevedo <emiacevedoposos@gmail.com>
2022-11-19 21:45:26 -0600

    Finished Transaction Page

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {LogInForm} from "../Pages/LogInForm";
 import {MainMenu} from "../Pages/MainMenu";
+import {Transaction} from "../Pages/Transaction";
 import {Route, Routes} from "react-router-dom";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<LogInForm />}/>
             <Route path="/menu" element={<MainMenu />}/>
+            <Route path="/transaction" element={<Transaction />}/>
         </Routes>
     );
 };
--- src/Pages/LogInForm/index.js
+++ src/Pages/LogInForm/index.js
@@ -9,7 +9,7 @@ const LogInForm = () => {
                 <p>¡Bienvenido a <span>Sintindir!</span></p>
                 <label className="cardLabel">Número de tarjeta</label>
                 <input type="text" minLength="16" maxLength="16" className="cardInput"/>
-                <lavbel className="pinLabel">NIP</lavbel>
+                <label className="pinLabel">NIP</label>
                 <input type="text" minLength="4" maxLength="4" className="nipInput"/>
                 <button type="submit">Ingresar</button>
             </form>
