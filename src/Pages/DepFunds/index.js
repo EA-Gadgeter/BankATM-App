a6e3a265bd8d43020bb32f2d0bd9ef45e08af7c0
SebasJacome <0237105@up.mx>
2022-11-19 21:41:52 -0600

    Finished DepFunds Page

---
+++ src/Pages/DepFunds/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./DepFunds.css";
+import exit from "../../img/cross-svgrepo-com.svg"
+
+const DepFunds = () =>{
+    return(
+        <div className="depFunds">
+            <div className="text-Dep">
+                <h1>Ingrese el monto a depositar</h1>
+            </div>
+            <div className="inputDep">
+                <form>
+                    <input type="number" placeholder="Ingrese su monto a depositar" className="moneyDep"/>
+                    <input type="submit" value="Enviar" className="dep-funds-submit"/>
+                </form>
+
+            </div>
+            <div className="exit-button">
+                <button>Cancelar</button>
+
+            </div>
+
+        </div>
+
+
+    );
+};
+
+export {DepFunds}
