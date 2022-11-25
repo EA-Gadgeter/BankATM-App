0d0499978b0b1780a92a7490c6c09f2638e91891
JArtBernal <0233936@up.edu.mx>
2022-11-24 22:48:59 -0600

    Finished withdraw

--- src/Pages/WithDraw/index.js
+++ src/Pages/WithDraw/index.js
@@ -2,10 +2,46 @@ import React from "react";
 
 import {MenuButton} from "../../Components/MenuButton";
 import {ProtectedRoute} from "../../Components/ProtectedRoute";
+import {serviceWithdraw} from "../../Services/serviceWithdraw";
+import {UserContext} from "../../Context/UserContext";
+import {AuthContext} from "../../Context/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 import "./WithDraw.css";
 
 const WithDraw = () => {
+
+    const {userFonds, setUserFonds} = React.useContext(UserContext);
+    const {userLoginInfo: {idAccount}} = React.useContext(AuthContext);
+    const [fondsToDraw, setFondsToDraw] = React.useState("");
+    const returnMenu = useNavigate();
+
+
+    const fondsOnChange = (event) => {
+        setFondsToDraw(event.target.value);
+    };
+
+    const withdrawSubmitted = (event) => {
+        event.preventDefault();
+        if (fondsToDraw > 0) {
+            serviceWithdraw(idAccount, fondsToDraw)
+                .then(response => {
+                    const {withdrawSuccesful, validMoney} = response;
+
+                    if(!withdrawSuccesful && !validMoney) {
+                        alert("No tienes suficientes fondos, ingresa otra cantidad porfavor.")
+                    } else if(!withdrawSuccesful && validMoney) {
+                        alert("Lo sentimos, no se pudo realiza el retiro, intentalo más tarde.")
+                    }
+                    else if(withdrawSuccesful && validMoney) {
+                        alert("Porfavor, retire su dinero.")
+                        setUserFonds(Number(userFonds) - Number(fondsToDraw));
+                        returnMenu("/menu");
+                    }
+                });
+        }
+    };
+
     return(
         <ProtectedRoute>
             <div className="withDraw">
@@ -14,11 +50,16 @@ const WithDraw = () => {
                 </div>
                 <div className="withDraw__info">
                     <p>Saldo Actual</p>
-                    <p>$ 999,999,999.00</p>
+                    <p>{`$${userFonds}`}</p>
                 </div>
                 <div className="withDraw__form">
-                    <form>
-                        <input type="number" placeholder="$ ..."/>
+                    <form onSubmit={withdrawSubmitted}>
+                        <input
+                            type="number" min="0"
+                            placeholder="$ ..."
+                            onChange={fondsOnChange}
+                            value={fondsToDraw}
+                        />
                         <button type="submit">Retirar</button>
                     </form>
                 </div>
